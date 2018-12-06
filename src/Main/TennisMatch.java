bded808bb2e5eae06764eaf7b80c29c8a4b65c02
ThomasP <trolldu25@live.fr>
2018-12-06 15:25:00 +0100

    Make base method

---
+++ src/Main/TennisMatch.java
@@ -0,0 +1,29 @@
+public class TennisMatch {
+    public TennisMatch() {
+    }
+
+    private void updateWithPointWonBy(Player player){
+
+    }
+
+    private String pointsForPlayer(Player player){
+
+        return null;
+    }
+
+    private int curretnSetNumber(){
+        return 0;
+    }
+
+    private int gamesInSetForPlyer(int i,Player player){
+        return 0;
+    }
+
+    private int gamesInCurrentSetForPlayer(Player player){
+        return 0;
+    }
+
+    private boolean isFinnished(){
+        return true;
+    }
+}
